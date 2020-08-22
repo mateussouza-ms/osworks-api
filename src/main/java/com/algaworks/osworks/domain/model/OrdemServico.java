0e0aa3544777d7879d6f4625590cf5b526e51504
Mateus Souza <mateus_souza_teles@hotmail.com>
2020-08-21 23:10:11 -0300

    Implementar as rotas de comentários
    Criação das classes para implentação das operações de adicionar e listar comentarios.

--- src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
+++ src/main/java/com/algaworks/osworks/domain/model/OrdemServico.java
@@ -2,6 +2,8 @@ package com.algaworks.osworks.domain.model;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
@@ -10,6 +12,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.NotBlank;
 
 @Entity
@@ -18,20 +21,23 @@ public class OrdemServico {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@ManyToOne
 	private Cliente cliente;
-	
+
 	@NotBlank
 	private String descricao;
-	
+
 	private BigDecimal preco;
-	
+
 	@Enumerated(EnumType.STRING)
 	private StatusOrdemServico status;
-	
+
 	private OffsetDateTime dataAbertura;
 	private OffsetDateTime dataFinalizacao;
+
+	@OneToMany(mappedBy = "ordemServico")
+	private List<Comentario> comentarios = new ArrayList<>();
 
 	public Long getId() {
 		return id;
@@ -87,6 +93,14 @@ public class OrdemServico {
 
 	public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
 		this.dataFinalizacao = dataFinalizacao;
+	}
+
+	public List<Comentario> getComentarios() {
+		return comentarios;
+	}
+
+	public void setComentarios(List<Comentario> comentarios) {
+		this.comentarios = comentarios;
 	}
 
 	@Override
